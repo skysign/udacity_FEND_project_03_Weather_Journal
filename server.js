925d7658449e254c9b2189fc43f8cb049c56d8ab
ByeongKeon Kim <skysign@gmail.com>
2021-08-28 18:10:29 +0900

    Implemented Weather API

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.post('/postProjectData', postProjectData);
+function postProjectData(req, res) {
+    projectData['temp'] = req.body.temp;
+    projectData['date'] = req.body.date;
+    projectData['content'] = req.body.content;
+
+    console.log(`postProjectData`);
+    console.log(`projectData: ${projectData['temp']}`);
+    console.log(`projectData: ${projectData['date']}`);
+    console.log(`projectData: ${projectData['content']}`);
+
+    res.send(projectData);
+}
+
+app.get('/getProjectData', getProjectData);
+function getProjectData(req, res) {
+    console.log(`getProjectData: ${projectData}`);
+    res.send(projectData);
+}
 
 // Setup Server
+const port = 80;
+const server = app.listen(port, () => {
+    console.log(`server port: ${port}`);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,100 @@
+// Personal API Key for OpenWeatherMap API
+const APIURL = 'http://api.openweathermap.org/data/2.5/weather';
+// ?q={city name}&appid={API key}
+const APIKey = 'c5c0688d265d12c69d78a2fcca878ada';
+
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Event listener to add function to existing HTML DOM element
+const btnGenerate = document.getElementById('generate');
+btnGenerate.addEventListener('click', clickBtnGenerate);
+
+/* Function called by event listener */
+function clickBtnGenerate(event) {
+    event.preventDefault();
+    btnGenerate.classList.remove('invalid');
+    const zip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;    
+
+    if (zip == '') {
+        console.log('clickBtnGenerate', 'no zip');
+        return;
+    }
+    
+    console.log('clickBtnGenerate', zip);
+
+    callWeatherAPI(zip)
+    .then(function(weatherData){
+        postProjectData('/postProjectData',
+                        {temp: weatherData.main.temp, date: newDate, content: content });
+    })
+    .then(function(){
+        getProjectData();
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+/* Function to GET Web API Data*/
+const callWeatherAPI = async(zip) => {
+    const res = await fetch(`${APIURL}?q=${zip}&appid=${APIKey}`);
+    try {
+        const weatherData = await res.json();
+        console.log('callWeatherAPI', weatherData);
+        return weatherData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+/* Function to POST data */
+const postProjectData = async(url = '', projectData = {}) => {
+    console.log('postProjectData', projectData.temp);
+    console.log('postProjectData', projectData.date);
+    console.log('postProjectData', projectData.content);
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            temp: projectData.temp,
+            date: projectData.date,
+            content: projectData.content
+        })
+    });
+
+    try {
+        const data = await response.json();
+        console.log('postProjectData', data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+/* Function to GET Project Data */
+const getProjectData = async() => {
+    const request = await fetch('/getProjectData');
+    try {
+        const projectData = await request.json();
+        console.log('getProjectData', projectData);
+        updateWeather(projectData);
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+function updateWeather(projectData) {
+    document.getElementById('date').innerHTML = projectData.date;
+    document.getElementById('temp').innerHTML = projectData.temp;
+    document.getElementById('content').innerHTML = projectData.content;
+}
